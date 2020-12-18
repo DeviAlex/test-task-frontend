3b656cc953f3ba789c462a124f9a7898400b6b4f
HELGA\Алексей <devilalex01@mail.ru>
2020-12-19 00:34:05 +0300

    add build version

--- build/webpack.build.conf.js
+++ build/webpack.build.conf.js
@@ -1,10 +1,22 @@
 const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
+const path = require('path')
+
+const PATHS = {
+  src: path.join(__dirname, '../src'),
+  dist: path.join(__dirname, '../dist/public'),
+  assets: 'assets/'
+}
 
 const buildWebpackConfig = merge(baseWebpackConfig, {
   // BUILD config
   mode: 'production',
-  plugins: []
+  plugins: [],
+  output: {
+    filename: `${PATHS.assets}js/[name].[hash].js`,
+    path: PATHS.dist,
+    publicPath: '/test-task-frontend/'
+  }
 })
 
 module.exports = new Promise((resolve, reject) => {
